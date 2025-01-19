19aacb1fa111b11e49d3f6efea33a1c18f8a9103
Svetlana <152962158+Ss1107S@users.noreply.github.com>
2025-01-19 13:00:02 +0300

    Add Problem_2703_Return_Length_of_Arguments_Passed
    Problem_2703_Return_Length_of_Arguments_Passed

---
+++ Problem_2703_Return_Length_of_Arguments_Passed.js
@@ -0,0 +1,15 @@
+class Problem_2703_Return_Length_of_Arguments_Passed {
+    //Language JavaScript
+    //Runtime 55ms
+    //Memory 48.9MB
+/**
+ * @param {...(null|boolean|number|string|Array|Object)} args
+ * @return {number}
+ */
+varargumentsLength = function(...args) {
+    return args.length;
+}
+/**
+ * argumentsLength(1, 2, 3); // 3
+ */
+}
